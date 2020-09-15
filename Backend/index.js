26359860de5fe9c4efe05b29fe52d9b663d2cc6c
Manjunath V A <manjunathappi68@gmail.com>
2020-09-15 16:11:43 +0530

    created separate folder for backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 require("dotenv").config();
 require("./config/mongoDBConfig");
 const colors = require("./helpers/colors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "../Frondend-UI/dist")));
 
 const port = process.env.NODE_PORT || 2000;
 
