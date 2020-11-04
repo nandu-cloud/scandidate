e9a306053e094d6cd10dcd04ddf1e7c4bd175306
Jaydeep1991 <jaydeeptomal@hotmail.com>
2020-11-04 11:59:51 +0530

    release 04-11-2020

--- Backend/components/scandidate/BGVSearch/bgvController.js
+++ Backend/components/scandidate/BGVSearch/bgvController.js
@@ -3,27 +3,31 @@ const colors = require("./../../../helpers/colors");
 const bgvDAL = require("./bgvDAL");
 
 module.exports.searchbgv = async (req, res, next) => {
-    let data = req.body;
-    console.log(data);
-    try {
-        let empData = await bgvDAL.searchBgvDataEmployee(data);
-        if (empData.length > 0) {
-            return res
-                .status(200)
-                .json({ status: 200, message: "Success", data: empData });
-        }
-        let stdData = await bgvDAL.searchBgvDataStudent(data);
-        if (stdData.length > 0) {
-            return res
-                .status(200)
-                .json({ status: 200, message: 'Success', data: stdData });
-        } else {
-            return res.status(404)
-                .json({ status: 404, message: 'No Such employee or student is found', data: [] });
-        }
-
-    } catch (err) {
-        console.log(colors.red, `${err}`);
-        return next(new AppError(err, 400));
+  let data = req.body;
+  console.log(data);
+  try {
+    let empData = await bgvDAL.searchBgvDataEmployee(data);
+    if (empData.length > 0) {
+      return res
+        .status(200)
+        .json({ status: 200, message: "Success", data: empData });
     }
+    let stdData = await bgvDAL.searchBgvDataStudent(data);
+    if (stdData.length > 0) {
+      return res
+        .status(200)
+        .json({ status: 200, message: "Success", data: stdData });
+    } else {
+      return res
+        .status(404)
+        .json({
+          status: 404,
+          message: "No Such employee or student is found",
+          data: [],
+        });
+    }
+  } catch (err) {
+    console.log(colors.red, `${err}`);
+    return next(new AppError(err, 400));
+  }
 };
--- Backend/components/scandidate/BGVSearch/bgvSearchRoute.js
+++ Backend/components/scandidate/BGVSearch/bgvSearchRoute.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const authJWT = require("../../../middlewares/authJWT");
 const bgvController = require("./bgvController");
 
+router.route("/").post(authJWT.verifyJWTToken, bgvController.searchbgv);
+
 router
-    .route("/")
-    .post(
-        authJWT.verifyJWTToken,
-        bgvController.searchbgv
-    );
+  .route("/searchbyid")
+  .post(authJWT.verifyJWTToken, bgvController.searchbgv);
 
 module.exports = router;
