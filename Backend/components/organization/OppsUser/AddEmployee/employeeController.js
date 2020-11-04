f7b41b3cfed88c8de7a265591bf1eb1c75f5a824
Jaydeep1991 <jaydeeptomal@hotmail.com>
2020-11-04 15:32:52 +0530

    release 04-11-2020

--- Backend/components/organization/OppsUser/AddEmployee/employeeController.js
+++ Backend/components/organization/OppsUser/AddEmployee/employeeController.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const AppError = require("./../../../../helpers/appError");
 const employeeDAL = require("./employeeDAL");
 const employeeDataValidator = require("./employeeValidator");
- 
+const colors = require("./../../../../helpers/colors");
 
 // Add Employee
 module.exports.addEmployeeMethod = async function (req, res, next) {
@@ -18,57 +18,57 @@ module.exports.addEmployeeMethod = async function (req, res, next) {
 
 //Get All Employee
 module.exports.getAllMethod = async function (req, res, next) {
-  const data = { organisationId: mongoose.Types.ObjectId(req.params.organisationId) };
-    try {
-      let employeeData = await employeeDAL.getAllUsers(data);
-      return res
-        .status(200)
-        .json({ status: "SUCCESS", message: null, data: employeeData });
-    } catch (err) {
-      console.log(colors.red, `getAllMethod err ${err}`);
-      return next(new AppError(err, 400));
-    }
+  const data = {
+    organisationId: mongoose.Types.ObjectId(req.params.organisationId),
   };
+  try {
+    let employeeData = await employeeDAL.getAllUsers(data);
+    return res
+      .status(200)
+      .json({ status: "SUCCESS", message: null, data: employeeData });
+  } catch (err) {
+    console.log(colors.red, `getAllMethod err ${err}`);
+    return next(new AppError(err, 400));
+  }
+};
 
-  // Get Employee By Id
+// Get Employee By Id
 module.exports.getEmployeeByIdMethod = async function (req, res, next) {
-    try {
-      const data = { _id: mongoose.Types.ObjectId(req.params.employeeId) };
-      let studentData = await employeeDAL.getEmplyeeById(data);
-      if (!studentData)
-        return next(new AppError("Employee does not exists!", 404));
-      return res.status(200).json({
-        status: "SUCCESS",
-        message: null,
-        data: studentData,
-      });
-    } catch (err) {
-  
-      return next(new AppError(err, 400));
-    }
-  };
+  try {
+    const data = { _id: mongoose.Types.ObjectId(req.params.employeeId) };
+    let studentData = await employeeDAL.getEmplyeeById(data);
+    if (!studentData)
+      return next(new AppError("Employee does not exists!", 404));
+    return res.status(200).json({
+      status: "SUCCESS",
+      message: null,
+      data: studentData,
+    });
+  } catch (err) {
+    return next(new AppError(err, 400));
+  }
+};
 
-  
-  // Update Student
+// Update Student
 module.exports.updateMethod = async function (req, res, next) {
-    const data = req.body;
-    try {
-      let result = await employeeDataValidator.updateEmployeeSchema.validateAsync(
-        data
-      );
-      data._id = mongoose.Types.ObjectId(req.params.employeeId);
-      const employeeExsits = await employeeDAL.getEmplyeeById(data);
-      if (!employeeExsits)
-        return next(new AppError("Employee does not exists!", 404));
-      result._id = mongoose.Types.ObjectId(req.params.employeeId);
-      result.updatedAt = new Date();
-      let studentData = await employeeDAL.updateEmployee(result);
-      return res.status(200).json({
-        status: "SUCCESS",
-        message: "Employee Data has been updated successfully",
-        data: studentData,
-      });
-    } catch (err) {
-      return next(new AppError(err, 400));
-    }
-  };
+  const data = req.body;
+  try {
+    let result = await employeeDataValidator.updateEmployeeSchema.validateAsync(
+      data
+    );
+    data._id = mongoose.Types.ObjectId(req.params.employeeId);
+    const employeeExsits = await employeeDAL.getEmplyeeById(data);
+    if (!employeeExsits)
+      return next(new AppError("Employee does not exists!", 404));
+    result._id = mongoose.Types.ObjectId(req.params.employeeId);
+    result.updatedAt = new Date();
+    let studentData = await employeeDAL.updateEmployee(result);
+    return res.status(200).json({
+      status: "SUCCESS",
+      message: "Employee Data has been updated successfully",
+      data: studentData,
+    });
+  } catch (err) {
+    return next(new AppError(err, 400));
+  }
+};
