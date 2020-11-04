adc5151f80e630c6a5f4374b610bcf9efde367cb
Jaydeep1991 <jaydeeptomal@hotmail.com>
2020-11-04 15:59:59 +0530

    release 04-11-2020

--- Backend/components/scandidate/BGVSearch/bgvController.js
+++ Backend/components/scandidate/BGVSearch/bgvController.js
@@ -6,13 +6,33 @@ module.exports.searchbgv = async (req, res, next) => {
   let data = req.body;
   try {
     let empData = await bgvDAL.searchBgvDataEmployee(data);
-    if (empData.length > 0) {
+    // if (empData.length > 0) {
+    //   return res
+    //     .status(200)
+    //     .json({ status: 200, message: "Success", data: empData });
+    // }
+    let stdData = await bgvDAL.searchBgvDataStudent(data);
+    // if (stdData.length > 0) {
+    //   return res
+    //     .status(200)
+    //     .json({ status: 200, message: "Success", data: stdData });
+    // } else {
+    //   return res.status(404).json({
+    //     status: 404,
+    //     message: "No Such employee or student is found",
+    //     data: [],
+    //   });
+    // }
+    if (empData.length > 0 && stdData.length > 0) {
+      let result = empData.concat(stdData);
+      return res
+        .status(200)
+        .json({ status: 200, message: "Success", data: result });
+    } else if (empData.length > 0 && !stdData.length > 0) {
       return res
         .status(200)
         .json({ status: 200, message: "Success", data: empData });
-    }
-    let stdData = await bgvDAL.searchBgvDataStudent(data);
-    if (stdData.length > 0) {
+    } else if (!empData.length > 0 && stdData.length > 0) {
       return res
         .status(200)
         .json({ status: 200, message: "Success", data: stdData });
