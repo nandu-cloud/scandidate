99a30acc7a8f7db9af4216872e2e2c883e80f677
Lalitha Venkata <lalitha@karins.in>
2021-01-04 13:03:46 +0530

    bgv search

--- Frondend-UI/src/app/services/bgv-search.service.ts
+++ Frondend-UI/src/app/services/bgv-search.service.ts
@@ -19,13 +19,20 @@ export class BgvSearchService {
 
  //Get bgv list
  public getAllBGVData(searchParams) : Observable<any>{
+  var userId = window.sessionStorage.getItem('ID');
   var searchData: { 'firstName':string,'dateOfBirth': string, 'phoneNumber': string ,'email': string ,'adharNumber':string} =
-                  { 'firstName':searchParams.firstName ==""? undefined : searchParams.firstName,'dateOfBirth': searchParams.dateOfBirth == ""? undefined : searchParams.dateOfBirth,'phoneNumber': searchParams.phoneNumber == ""? undefined : searchParams.phoneNumber,'email':searchParams.email == ""?undefined : searchParams.email,'adharNumber':searchParams.adharNumber == ""?undefined : searchParams.adharNumber};       
+                  { 'firstName':searchParams.firstName ==""? undefined : searchParams.firstName,
+                    'dateOfBirth': searchParams.dateOfBirth == ""? undefined : searchParams.dateOfBirth,
+                    'phoneNumber': searchParams.phoneNumber == ""? undefined : searchParams.phoneNumber,
+                    'email':searchParams.email == ""?undefined : searchParams.email,
+                    'adharNumber':searchParams.adharNumber == ""?undefined : searchParams.adharNumber
+                  };       
                   
                   
    //Object.keys(searchData).map(k => searchData[k] = searchData[k].trim());
   console.log(searchData); 
-  return this.http.post(this.baseUrl + '/api/scandidate/bgvsearch' ,searchData
+  return this.http.post(this.baseUrl + '/api/scandidate/bgvsearch/'+ userId, searchData
+  // return this.http.post(`${this.baseUrl}/api/scandidate/bgvsearch/${userId}/${searchData}`)
   );
   }
 
