2b289be9c206e4e9b582e6d702e30bb77fa5e78b
Jaydeep1991 <jaydeeptomal@hotmail.com>
2021-04-19 15:50:59 +0530

    email responsiveness

--- Backend/components/contactus/contactUsController.js
+++ Backend/components/contactus/contactUsController.js
@@ -16,8 +16,8 @@ module.exports.savecontactus = async (req, res, next) => {
         template.toemail = process.env.FIRSTPERSONID;
         template.logo = `${process.env.FRONT_END_URL}/assets/images/logo1.png`;
         template.toemailsecondary = process.env.SECONDPERSONID;
-        // template.subject = "Contact us form details";
-        template.subject = "Test contact us form details";
+        template.subject = "Contact us form details";
+        // template.subject = "Test contact us form details";
 
 
         try {
