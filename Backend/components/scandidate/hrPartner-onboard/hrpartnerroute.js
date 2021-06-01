d6d8b0c0413792fc4efb1d90a2af3e7194977dd8
Jaydeep1991 <jaydeeptomal@hotmail.com>
2021-06-01 13:33:10 +0530

    hrpartner folder structure change

--- Backend/components/scandidate/hrPartner-onboard/hrpartnerroute.js
+++ Backend/components/scandidate/hrPartner-onboard/hrpartnerroute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authJWT = require("../../middlewares/authJWT");
+const authJWT = require("../../../middlewares/authJWT");
 const hrpartnerController = require("./hrpartnerController");
 
 router
