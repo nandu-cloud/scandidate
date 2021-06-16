598e1a8783de79c6ff88cfe4937ead71b6cf76d6
Jaydeep1991 <jaydeeptomal@hotmail.com>
2021-06-16 18:15:00 +0530

    show candidate format change

--- Backend/components/HRPartner/AddCandidate/addCandidateController.js
+++ Backend/components/HRPartner/AddCandidate/addCandidateController.js
@@ -88,19 +88,19 @@ module.exports.showCandidate = async (req, res, next) => {
     let emp = await empDAL.showEmployee({
       hrorganisationId: req.params.hrorganisationId,
     });
-    let newEmployee = [];
-    let uniqueObject = {};
+    let newEmployeeArray = [];
+    let uniqueEmployee = {};
     for (let i in emp) {
       empEmail = emp[i]["email"];
-      uniqueObject[empEmail] = emp[i];
+      uniqueEmployee[empEmail] = emp[i];
     }
-    for (i in uniqueObject) {
-      newEmployee.push(uniqueObject[i]);
+    for (i in uniqueEmployee) {
+      newEmployeeArray.push(uniqueEmployee[i]);
     }
     let stud = await stdDAL.showStudent({
       hrorganisationId: req.params.hrorganisationId,
     });
-    let result = newEmployee.concat(stud);
+    let result = newEmployeeArray.concat(stud);
 
     let resultData = result.sort((a, b) => {
       return b.createdAt - a.createdAt;
@@ -127,9 +127,100 @@ module.exports.showCandidateById = async (req, res, next) => {
     var studentData = await canddidateDAL.fetchStudent({ email: email });
     var empData = await canddidateDAL.fetchEmployee({ email: email });
     var totalData = studentData.concat(empData);
+    var empBio = totalData[0];
+    var bio = {
+      firstName: empBio.firstName,
+      lastName: empBio.lastName,
+      email: empBio.email,
+      adharNumber: empBio.adharNumber,
+      phoneNumber: empBio.phoneNumber,
+      panNumber: empBio.panNumber,
+      dateOfBirth: empBio.dateOfBirth,
+      address: empBio.address,
+      addedById: empBio.addedById,
+      hrorganisationId: empBio.hrorganisationId,
+    };
+    var verification = {
+      dateOfVerification: empBio.dateOfVerification,
+      personalIdentity: empBio.personalIdentity,
+      criminal: empBio.criminal,
+      verificationAddress: empBio.verificationAddress,
+      drugsAndSubstanceAbuse: empBio.drugsAndSubstanceAbuse,
+      salarySlipCTCdocument: empBio.salarySlipCTCdocument,
+    };
+    var candidate = [];
+    for (var i = 0; i < totalData.length; i++) {
+      var org = {
+        organizationName: totalData[i].organizationName,
+        nameofFeedbackProvider: totalData[i].nameofFeedbackProvider,
+        designationOfFeedbackProvider:
+          totalData[i].designationOfFeedbackProvider,
+        exitDate: totalData[i].exitDate,
+        professionalExperience: totalData[i].professionalExperience,
+        employeeId: totalData[i].employeeId,
+        selfDriven: totalData[i].selfDriven,
+        creativity: totalData[i].creativity,
+        initiative: totalData[i].initiative,
+        dealConstructivelyWithPressure:
+          totalData[i].dealConstructivelyWithPressure,
+        quality: totalData[i].quality,
+        discipline: totalData[i].discipline,
+        productKnowledge: totalData[i].productKnowledge,
+        industryKnowledge: totalData[i].industryKnowledge,
+        awards: totalData[i].awards,
+        building: totalData[i].building,
+        dateOfJoining: totalData[i].dateOfJoining,
+        role: totalData[i].role,
+        department: totalData[i].department,
+        informalOrganizationSenseOfBelonging:
+          totalData[i].informalOrganizationSenseOfBelonging,
+        workIndependenty: totalData[i].workIndependenty,
+        volume: totalData[i].volume,
+        consistency: totalData[i].consistency,
+        punctuality: totalData[i].punctuality,
+        academicKnowledge: totalData[i].academicKnowledge,
+        communicationSkills: totalData[i].communicationSkills,
+        stakeholder: totalData[i].stakeholder,
+        discrepancyDocuments: totalData[i].discrepancyDocuments,
+        compliencyDiscrepancy: totalData[i].compliencyDiscrepancy,
+        warning: totalData[i].warning,
+        showCausedIssue: totalData[i].showCausedIssue,
+        suspension: totalData[i].suspension,
+        termination: totalData[i].termination,
+        keySkills: totalData[i].keySkills,
+        empThrive: totalData[i].empThrive,
+        inLeadership: totalData[i].inLeadership,
+        otherInfo: totalData[i].otherInfo,
+        rehireAgain: totalData[i].rehireAgain,
+        reasonForSerperation: totalData[i].reasonForSerperation,
+        originalFilename: totalData[i].originalFilename,
+        status: totalData[i].status,
+      };
+      candidate.push(org);
+    }
+    var candidateData = {
+      firstName: empBio.firstName,
+      lastName: empBio.lastName,
+      email: empBio.email,
+      adharNumber: empBio.adharNumber,
+      phoneNumber: empBio.phoneNumber,
+      panNumber: empBio.panNumber,
+      dateOfBirth: empBio.dateOfBirth,
+      address: empBio.address,
+      addedById: empBio.addedById,
+      hrorganisationId: empBio.hrorganisationId,
+
+      dateOfVerification: empBio.dateOfVerification,
+      personalIdentity: empBio.personalIdentity,
+      criminal: empBio.criminal,
+      verificationAddress: empBio.verificationAddress,
+      drugsAndSubstanceAbuse: empBio.drugsAndSubstanceAbuse,
+      salarySlipCTCdocument: empBio.salarySlipCTCdocument,
+      candidate: candidate,
+    };
     return res
       .status(200)
-      .json({ status: 200, message: null, data: totalData });
+      .json({ status: 200, message: null, data: candidateData });
   } catch (err) {
     return next(new AppError(err, 422));
   }
@@ -137,6 +228,7 @@ module.exports.showCandidateById = async (req, res, next) => {
 
 module.exports.updateCandidateData = async (req, res, next) => {
   const { bio, candidate, verification } = req.body;
+  const empId = req.params.candidateId;
   var data = [];
   for (var d of candidate) {
     if (d.hasOwnProperty("organizationName")) {
@@ -195,7 +287,7 @@ module.exports.updateCandidateData = async (req, res, next) => {
         }
       } else {
         d.bgvCandidate = true;
-        let stdvalid = await saveNowValidator.addStudentSchema.validateAsync(d);
+        let stdvalid = await stdValidator.addStudentSchema.validateAsync(d);
         stdvalid._id = mongoose.Types.ObjectId(empId);
         var saveCandidate = await saveNowStdDAL.updateStudent({
           _id: mongoose.Types.ObjectId(empId),
