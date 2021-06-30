84242222a9c85bc2d5c2636c1b8b0eca981daac6
Jaydeep1991 <jaydeeptomal@hotmail.com>
2021-06-30 14:01:24 +0530

    hrpartner value change

--- Backend/components/scandidate/user/userModel.js
+++ Backend/components/scandidate/user/userModel.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: [true, "role cannot be empty"],
-    enum: ["SCANDIDATE", "ORGANIZATION", "INSTITUTION", "HRPARTNER"],
+    enum: ["SCANDIDATE", "ORGANIZATION", "INSTITUTION", "AGENCY"],
   },
   subRole: {
     type: String,
